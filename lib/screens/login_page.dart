450da7724141d9aaeb6ce9031de37ec38f67ffe1
Bilal Kaya <bilalkayaa0@gmail.com>
2023-10-30 18:22:59 +0300

    login sayfası olusturuldu

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isHidden = true;
+  void Chngvisibility() {
+    setState(() {
+      isHidden = !isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Expanded(
+              flex: 4,
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(120),
+                    )),
+                child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 120,
+                        ),
+                        Container(
+                          child: Text(
+                            "EasyKey",
+                            style: TextStyle(fontSize: 32, letterSpacing: 2),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 70,
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical:
+                                      8.0), // Aralık eklemek için margin kullanıyoruz
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    labelText: "Kullanıcı adı",
+                                    hintText: "",
+                                    prefixIcon: Icon(Icons.person)),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.symmetric(vertical: 8.0),
+                              child: TextFormField(
+                                obscureText: isHidden,
+                                decoration: InputDecoration(
+                                  labelText: "Şifre",
+                                  hintText: "",
+                                  prefixIcon: Icon(Icons.key),
+                                  suffixIcon: IconButton(
+                                      onPressed: Chngvisibility,
+                                      icon: isHidden
+                                          ? Icon(Icons.visibility)
+                                          : Icon(Icons.visibility_off)),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "Henüz üye değil misin?",
+                                    textAlign: TextAlign.right,
+                                  ),
+                                  TextButton(
+                                    onPressed: () => "",
+                                    child: Text(
+                                      "Tıkla Üye ol!",
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.symmetric(vertical: 8.0),
+                              child: TextButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  'Giriş yap',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+              )),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          )
+        ],
+      ),
+    );
+  }
+}
